a9f17a1161a8802abb6b95b36a2d760c1de2a458
vincent desmouceaux <vincentdesmont@MacBook-Pro-de-vincent.local>
2023-01-30 12:11:45 +0100

    arraysAddition

---
+++ arrayAddition.js
@@ -0,0 +1,24 @@
+const arrayAddition = (tab) => {
+  const sortedTab = tab.sort((a, b) => {
+    return a - b;
+  });
+
+  console.log(sortedTab);
+
+  const max = sortedTab.pop();
+  let result = 0;
+
+  for (let i = 0; i < sortedTab.length; i++) {
+    result = result + sortedTab[i];
+  }
+  console.log(sortedTab);
+  console.log(max);
+
+  if (result === max) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(arrayAddition([1, 2, 3, 12, 6]));
+console.log(arrayAddition([1, 2, 3, 4]));
