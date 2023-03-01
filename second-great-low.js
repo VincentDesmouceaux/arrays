54d9e1e60905a146e98b12f488c402602b07ac2f
Vincent Desmouceaux <114471831+VincentDesmouceaux@users.noreply.github.com>
2023-03-01 16:52:42 +0100

    update

---
+++ second-great-low.js
@@ -0,0 +1,26 @@
+const secondGreatLow = (values) => {
+  for (let i = 0; i < values.length; i++) {
+    if (isNaN(values[i]) === true) {
+      return "error";
+    }
+  }
+
+  let uniqueValues = [];
+
+  for (let i = 0; i < values.length; i++) {
+    if (uniqueValues.indexOf(values[i]) === -1) {
+      uniqueValues.push(values[i]);
+    }
+  }
+
+  if (uniqueValues.length === 1) {
+    return [values[0], values[0]];
+  }
+  uniqueValues.sort(function (a, b) {
+    return b - a;
+  });
+
+  return [uniqueValues[uniqueValues.length - 2], uniqueValues[1]];
+};
+
+console.log(secondGreatLow([1, 42, 47, 238]));
