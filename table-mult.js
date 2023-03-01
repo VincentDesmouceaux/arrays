36911894d445a5ec210e0045d828de49c04c5086
Vincent Desmouceaux <114471831+VincentDesmouceaux@users.noreply.github.com>
2023-03-01 12:51:56 +0100

    update

---
+++ table-mult.js
@@ -0,0 +1,24 @@
+const TableMult = (num) => {
+  if (typeof num !== "number") {
+    return "Vous devez entrer un nombre";
+  }
+
+  if (num < 0) {
+    return "Vous devez entrer un nombre positif";
+  }
+
+  if (Number.isInteger(num) === false) {
+    return "Vous devez entrer un nombre entier";
+  }
+
+  let results = [];
+
+  for (let i = 1; i <= 10; i++) {
+    let result = i * num;
+    results.push(i + "*" + num + "=" + result);
+  }
+
+  return results.join("\n");
+};
+
+console.log(TableMult(4));
