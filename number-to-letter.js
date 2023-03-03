824c2a87b5bc87620db0b25984f87c8f6f541c3d
Vincent Desmouceaux <114471831+VincentDesmouceaux@users.noreply.github.com>
2023-03-03 11:45:34 +0100

    number-to-letter.js

--- number-to-letter.js
+++ number-to-letter.js
@@ -66,5 +66,91 @@ const handleTens = (num) => {
       }
       res += handleJoiner(num, currentNum, zero2sixteen[currentNum % 10]);
     }
+  } else if (tens >= 6 && tens < 8) {
+    res += twenty2sixty[6 - 2];
+    if (currentNum - 60 > 16) {
+      res += "-" + zero2sixteen[10] + "-" + zero2sixteen[currentNum - 70];
+    } else {
+      if (currentNum !== 60) {
+        if (currentNum === 61 || currentNum === 71) {
+          res += " ";
+        } else {
+          res += "-";
+        }
+        res += handleJoiner(num, currentNum, zero2sixteen[currentNum - 60]);
+      }
+    }
+  } else if (tens >= 8) {
+    res += zero2sixteen[4] + "-" + twenty2sixty[0];
+    if (currentNum - 80 > 16) {
+      res += "-" + zero2sixteen[10] + "-" + zero2sixteen[currentNum - 90];
+    } else {
+      if (currentNum !== 80) {
+        res += "-" + zero2sixteen[currentNum - 80];
+      } else {
+        res += "s";
+      }
+    }
   }
+  return res;
 };
+
+const handleHundreds = (num) => {
+  let res = "";
+
+  if (num % 1000 > 0) {
+    res += " ";
+  }
+
+  currentNum = num % 1000;
+  let hundreds = Math.floor(currentNum / 100);
+
+  if (hundreds === 0) {
+    return res;
+  } else if (hundreds === 1) {
+    res += hundred2thousand[0];
+    return res;
+  }
+
+  res += zero2sixteen[hundreds] + " " + hundred2thousand[0];
+
+  if (currentNum % 100 === 0) {
+    res += "s";
+  }
+
+  return res;
+};
+
+const handleThousands = (num) => {
+  let thousands = Math.floor(num / 1000);
+
+  if (thousands === 0) {
+    return "";
+  } else if (thousands === 1) {
+    return hundred2thousand[1];
+  }
+  return zero2sixteen[thousands] + " " + hundred2thousand[1];
+};
+
+const NumberToLetters = (num) => {
+  let res = "";
+
+  if (num === "" || isNaN(num) || num < 0 || num > 9999 || num % 1 !== 0) {
+    res += "Paramètre invalide";
+
+    return res;
+  }
+  num = parseInt(num);
+
+  if (num === 0) {
+    res += zero2sixteen[0];
+    return res;
+  }
+  res += handleThousands(num);
+  res += handleHundreds(num);
+  res += handleTens(num);
+
+  return res.replace("  ", " ").trim();
+};
+
+console.log(NumberToLetters(1234));
